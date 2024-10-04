1e98c6897aafb8d174cc83cfafc3d8f38cab35fe
Franklin Marcelo Reyes Banegas <freyeshn96@outlook.es>
2024-10-04 07:36:27 -0600

    chore: added AssemblyReference.

---
+++ SweetAuth.IdentityProvider.Domain/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace SweetAuth.IdentityProvider.Domain
+{
+    public class AssemblyReference
+    {
+        public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+    }
+}
