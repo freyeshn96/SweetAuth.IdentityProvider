80858b320cfb49c84530eb3b31fbf794cf8f3a50
Franklin Marcelo Reyes Banegas <freyeshn96@outlook.es>
2024-10-04 07:18:49 -0600

    feat: added repository pattern

---
+++ SweetAuth.IdentityProvider.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using SweetAuth.IdentityProvider.Domain.Entities;
+
+namespace SweetAuth.IdentityProvider.Domain.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IRepository<TEntity> WithTrackingValue(bool trackingValue);
+        Task<TEntity> InsertAsync(TEntity entity, CancellationToken token);
+        TEntity Insert(TEntity entity);
+        Task<List<TEntity>> InsertRangeAsync(IEnumerable<TEntity> entities, CancellationToken token);
+        List<TEntity> InsertRange(IEnumerable<TEntity> entities);
+        Task<List<TEntity>> GetAllAsync(CancellationToken token);
+        List<TEntity> GetAll();
+        Task<TEntity?> GetByIdAsync(int id, CancellationToken token);
+        TEntity? GetById(int id);
+        Task<bool> ExistsAsync(int id, CancellationToken cancellationToken);
+        bool Exists(int id);
+        Task<int> SaveChangesAsync(CancellationToken token);
+        int SaveChanges();
+        TEntity Update(TEntity entity);
+        List<TEntity> UpdateRange(IEnumerable<TEntity> entities);
+        TEntity SoftDelete(TEntity entity);
+    }
+}
---
+++ SweetAuth.IdentityProvider.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SweetAuth.IdentityProvider.Domain.Entities;
+using SweetAuth.IdentityProvider.Domain.Interfaces;
+
+namespace SweetAuth.IdentityProvider.Infrastructure.Repositories
+{
+    public class Repository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : BaseEntity
+        where TContext : DbContext
+    {
+        private readonly TContext _dbContext;
+        private bool _trackingEntities = true;
+        
+        public Repository(TContext dbContext) {
+            this._dbContext = dbContext;
+        }
+
+        public async Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            DbSet<TEntity> entity = this._dbContext.Set<TEntity>();
+
+            if (!this._trackingEntities)
+            {
+                IQueryable<TEntity> queryable =
+                    this._dbContext.Set<TEntity>().AsNoTracking();
+                
+                return await queryable.ToListAsync(cancellationToken); 
+            }
+            
+            return await entity.ToListAsync(cancellationToken);
+        }
+        
+        public List<TEntity> GetAll()
+        {
+            DbSet<TEntity> entity = this._dbContext.Set<TEntity>();
+
+            if (!this._trackingEntities)
+            {
+                IQueryable<TEntity> queryable =
+                    this._dbContext.Set<TEntity>().AsNoTracking();
+                
+                return queryable.ToList(); 
+            }
+            
+            return entity.ToList();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            if (!this._trackingEntities)
+            {
+                IQueryable<TEntity> queryable =
+                    this._dbContext.Set<TEntity>().AsNoTracking();
+                
+                return await queryable.FirstOrDefaultAsync(q => q.Id == id, cancellationToken); 
+            }
+            
+            return await this._dbContext.Set<TEntity>().FindAsync(id, cancellationToken);
+        }
+        
+        public TEntity? GetById(int id)
+        {
+            if (!this._trackingEntities)
+            {
+                IQueryable<TEntity> queryable =
+                    this._dbContext.Set<TEntity>().AsNoTracking();
+                
+                return queryable.FirstOrDefault(q => q.Id == id); 
+            }
+            
+            return this._dbContext.Set<TEntity>().Find(id);
+        }
+        
+        public async Task<bool> ExistsAsync(int id, CancellationToken cancellationToken)
+        {
+            return await this._dbContext.Set<TEntity>().AnyAsync(e => e.Id == id, cancellationToken);
+        }
+        
+        public bool Exists(int id)
+        {
+            return this._dbContext.Set<TEntity>().Any(e => e.Id == id);
+        }
+        
+        public IRepository<TEntity> WithTrackingValue(bool trackingValue)
+        {
+            this._trackingEntities = trackingValue;
+
+            return this;
+        }
+
+        public async Task<TEntity> InsertAsync(TEntity entity, CancellationToken cancellationToken)
+        {
+            await this._dbContext.Set<TEntity>().AddAsync(entity, cancellationToken);
+
+            return entity;
+        }
+        
+        public TEntity Insert(TEntity entity)
+        {
+            this._dbContext.Set<TEntity>().Add(entity);
+
+            return entity;
+        }
+
+        public async Task<List<TEntity>> InsertRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken)
+        {
+            List<TEntity> entitiesList = entities.ToList();
+            await this._dbContext.Set<TEntity>().AddRangeAsync(entitiesList, cancellationToken);
+
+            return entitiesList;
+        }
+        
+        public List<TEntity> InsertRange(IEnumerable<TEntity> entities)
+        {
+            List<TEntity> entitiesList = entities.ToList();
+            this._dbContext.Set<TEntity>().AddRangeAsync(entitiesList);
+
+            return entitiesList;
+        }
+
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await this._dbContext.SaveChangesAsync(cancellationToken);
+        }
+        
+        public int SaveChanges()
+        {
+            return this._dbContext.SaveChanges();
+        }
+
+        public TEntity SoftDelete(TEntity entity)
+        {
+            entity.IsDeleted = true;
+            
+            return entity;
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            this._dbContext.Set<TEntity>().Update(entity);
+
+            return entity;
+        }
+
+        public List<TEntity> UpdateRange(IEnumerable<TEntity> entities)
+        {
+            List<TEntity> entitiesList = entities.ToList();
+            this._dbContext.Set<TEntity>().UpdateRange(entitiesList);
+
+            return entitiesList;
+        }
+    }
+}
